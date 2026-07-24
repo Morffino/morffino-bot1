a8a31b3c851e60fabb573c05eefa7a247c389485
Morffino <130293159+Morffino@users.noreply.github.com>
2026-07-24 12:23:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ async def setup_tickets(ctx: commands.Context):
 
 
 # Токен вашего бота (ЗАМЕНИТЕ НА НОВЫЙ ТОКЕН ИЗ DISCORD DEVELOPER PORTAL)
-TOKEN = "MTUzMDAwMTkxNzMxNTM4NzQ2Mw.GHTSxV.55jukT-TrFaIgQSGiW3mTHAeoW6olz4ZLRoveA"
+TOKEN = "MTUzMDAwMTkxNzMxNTM4NzQ2Mw.GcmeH8.KFWgn8BscO7tx1CkA-WZ3uzoHhsdx_ZVsTyWI8"
 
 if __name__ == "__main__":
     bot.run(TOKEN)
